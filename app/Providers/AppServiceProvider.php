8446f9598dd4a3b3de70133d7d20b943f2b96cde
Sob <zobirin90@gmail.com>
2023-08-17 19:51:12 +0800

    installed filament

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Builder;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        /// Update defaultStringLength
+        Builder::defaultStringLength(101);
     }
 }
